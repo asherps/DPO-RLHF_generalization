afcf01e36f2e3b05ec02d1f03a4415d3e1c1248a
misha <mikejg7@gmail.com>
2024-05-10 00:17:18 +0000

    add misha's rlhf train

--- train_rlhf.py
+++ train_rlhf.py
@@ -1,10 +1,12 @@
 from trl import PPOConfig, PPOTrainer
 import utils
 from transformers import (
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
     AutoModelForCausalLM,
-    AutoModelForCausalLMWithValueHead,
+    AutoTokenizer,
+    AutoModel,
+    Trainer,
+    TrainingArguments,
+    BertModel,
     pipeline,
     AutoModelForSequenceClassification,
 )
@@ -19,7 +21,6 @@ import trl
 
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
-
 
 def setup_logging(hps: Dict[str, Any]):
     # Choose logging and checkpoint saving directory
@@ -79,118 +80,47 @@ def main():
     hps["debug"] = args.debug
     if hps["debug"]:
         hps["training_kwargs"]["max_steps"] = 5
-    logdir = setup_logging(hps)
 
-    # load tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    tokenizer.pad_token = tokenizer.eos_token
-    model = AutoModelForCausalLMWithValueHead.from_pretrained(
-        model, torch_dtype=t.bfloat16
+
+
+    # load model
+    tokenizer, model = utils.load_model(
+        hps["model"],
+        reward_model=False,
+        eval=False,
     )
-    model.to(t.device(device))
-    model.config.pad_token_id = model.config.eos_token_id
+
+    model = trl.AutoModelForCausalLMWithValueHead.from_pretrained(model)
 
     # Load and process dataset. Make eval set smaller for speed reasons.
     dataset = utils.load_dataset(tokenizer, **hps["dataset"], debug=True)
-    print(dataset)
-    print(dataset.keys())
-    breakpoint()
     test_size = min(len(dataset["test"]), 2_000)
     dataset["test"] = dataset["test"].shuffle(seed=42).select(range(test_size))
-
-    config = PPOConfig(
-        model_name="mistralai/Mistral-7B-Instruct-v0.2",
-        learning_rate=1.41e-5,
-    )
-
-    sent_kwargs = {
-        "return_all_scores": True,
-        "function_to_apply": "none",
-        "batch_size": 16,
-    }
-
-    def tokenize(sample):
-        sample["input_ids"] = tokenizer.encode(sample["prompt"])
-        return sample
-
-    dataset = dataset.map(tokenize, batched=False)
-
-    ppo_trainer = PPOTrainer(
-        model=model,
-        config=config,
-        dataset=dataset["train"],
-        tokenizer=tokenizer,
-    )
-
-    # load reward model
-    reward_model = AutoModelForSequenceClassification.from_pretrained(
-        hps["reward_model_path"]
-    )
-
-    generation_kwargs = {
-        "min_length": -1,
-        "top_k": 0.0,
-        "top_p": 1.0,
-        "do_sample": True,
-        "pad_token_id": tokenizer.eos_token_id,
-    }
-
-    # wandb.init()
-
-    epochs = 10
-    for _ in tqdm(range(epochs), "epoch: "):
-        for batch in tqdm(ppo_trainer.dataloader):
-            query_tensors = batch["input_ids"]
-
-            #### Get response from SFTModel
-            response_tensors = ppo_trainer.generate(query_tensors, **generation_kwargs)
-            batch["response"] = [
-                tokenizer.decode(r.squeeze()) for r in response_tensors
-            ]
-
-            #### Compute reward score
-            texts = [
-                q + t + r
-                for q, t, r in zip(batch["query"], "Assistant: ", batch["response"])
-            ]
-            pipe_outputs = reward_model(texts)
-            print(texts[0])
-            breakpoint()
-            rewards = [t.tensor(output[1]["score"]) for output in pipe_outputs]
-
-            #### Run PPO step
-            stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
-            ppo_trainer.log_stats(stats, batch, rewards)
-
-    #### Save model
-    ppo_trainer.save_pretrained("post_ppo_model")
-
-
-if __name__ == "__main__":
-    main()
+    # Setting logging
+    logdir = setup_logging(hps)
 
     # I think PPO trainer fine tunes already, so we don't need this
-    #     peft_config = LoraConfig(
+#     peft_config = LoraConfig(
+    
+#     task_type=TaskType.CAUSAL_LM, inference_mode=False, r=32, lora_alpha=16, lora_dropout=0.1,
+# ) # create LoRA config for the finetuning
 
-    #     task_type=TaskType.CAUSAL_LM, inference_mode=False, r=32, lora_alpha=16, lora_dropout=0.1,
-    # ) # create LoRA config for the finetuning
+#     model = get_peft_model(model, peft_config) # create a model ready for LoRA finetuning
 
-    #     model = get_peft_model(model, peft_config) # create a model ready for LoRA finetuning
+#     tokenizer.pad_token = tokenizer.eos_token # need this because tokenizer doesn't have default padding
 
-    #     tokenizer.pad_token = tokenizer.eos_token # need this because tokenizer doesn't have default padding
-
-    #     # fine tune!
-    #     training_args = TrainingArguments(
-    #         output_dir="./results",
-    #         num_train_epochs=3,
-    #         per_device_train_batch_size=1,
-    #         per_device_eval_batch_size=2,
-    #         warmup_steps=500,
-    #         weight_decay=0.01,
-    #         logging_dir=logdir,
-    #         logging_steps=10,
-    #         learning_rate = 1e-3,
-    #     )
+#     # fine tune!
+#     training_args = TrainingArguments(
+#         output_dir="./results",
+#         num_train_epochs=3,
+#         per_device_train_batch_size=1,
+#         per_device_eval_batch_size=2,
+#         warmup_steps=500,
+#         weight_decay=0.01,
+#         logging_dir=logdir,
+#         logging_steps=10,
+#         learning_rate = 1e-3,
+#     )
 
 #     trainer = Trainer(
 #         model=model,
