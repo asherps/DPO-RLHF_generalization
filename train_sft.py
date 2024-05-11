45e4f9b4aa1726566cfbba7db799fb87eb50101d
Asher Parker-Sartori <aparkersartori@outlook.com>
2024-05-11 19:36:12 -0400

    microcommit

--- train_sft.py
+++ train_sft.py
@@ -4,7 +4,7 @@ from transformers import (
     AutoTokenizer,
     AutoModelForSequenceClassification,
     BitsAndBytesConfig,
-    TrainingArguments
+    TrainingArguments,
 )
 import yaml
 import getpass
@@ -66,7 +66,7 @@ def main():
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=t.bfloat16
+        bnb_4bit_compute_dtype=t.bfloat16,
     )
 
     # load model
@@ -74,7 +74,7 @@ def main():
         hps["model"],
         reward_model=False,
         eval=False,
-        quantized =True, 
+        quantized=True,
         bnb_config=bnb_config,
     )
     dataset = utils.load_dataset(tokenizer, **hps["dataset"], debug=False, sft=True)
@@ -86,38 +86,40 @@ def main():
     print(memory())
 
     args = TrainingArguments(
-    output_dir="sft_model", # directory to save and repository id
-    num_train_epochs=1,                     # number of training epochs
-    per_device_train_batch_size=4,          # batch size per device during training
-    gradient_accumulation_steps=2,          # number of steps before performing a backward/update pass
-    gradient_checkpointing=True,            # use gradient checkpointing to save memory
-    optim="adamw_torch_fused",              # use fused adamw optimizer
-    logging_steps=10,                       # log every 10 steps
-    save_strategy="epoch",                  # save checkpoint every epoch
-    learning_rate=2e-4,                     # learning rate, based on QLoRA paper
-    bf16=True,                              # use bfloat16 precision
-    tf32=True,                              # use tf32 precision
-    max_grad_norm=0.3,                      # max gradient norm based on QLoRA paper
-    warmup_ratio=0.03,                      # warmup ratio based on QLoRA paper
-)
+        output_dir="sft_model",  # directory to save and repository id
+        num_train_epochs=1,  # number of training epochs
+        per_device_train_batch_size=4,  # batch size per device during training
+        gradient_accumulation_steps=2,  # number of steps before performing a backward/update pass
+        gradient_checkpointing=True,  # use gradient checkpointing to save memory
+        optim="adamw_torch_fused",  # use fused adamw optimizer
+        logging_steps=10,  # log every 10 steps
+        save_strategy="epoch",  # save checkpoint every epoch
+        learning_rate=2e-4,  # learning rate, based on QLoRA paper
+        bf16=True,  # use bfloat16 precision
+        tf32=True,  # use tf32 precision
+        max_grad_norm=0.3,  # max gradient norm based on QLoRA paper
+        warmup_ratio=0.03,  # warmup ratio based on QLoRA paper
+    )
     trainer = SFTTrainer(
         model,
-        tokenizer= tokenizer,
+        tokenizer=tokenizer,
         train_dataset=dataset["train"].select(range(40000)),
-        args = args,
+        args=args,
         dataset_text_field="prompt",
-        dataset_batch_size = 1000,
-        eval_dataset = dataset["test"], 
+        dataset_batch_size=1000,
+        eval_dataset=dataset["test"],
         peft_config=hps["peft_config_class"](**hps["peft_config_kwargs"]),
-        packing = True, 
-        max_seq_length = 3072
-        )
+        packing=True,
+        max_seq_length=3072,
+    )
     trainer.train()
 
     trainer.save_model()
     wandb.finish()
     del model
     del trainer
-    torch.cuda.empty_cache()
+    t.cuda.empty_cache()
+
+
 if __name__ == "__main__":
     main()
