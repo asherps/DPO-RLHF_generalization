a46ee476d7f9f25a510dc22d3e5e44c59422e503
Asher <aparkersartori@outlook.com>
2024-05-12 21:12:01 +0000

    updated ood evals

--- ood_evals.py
+++ ood_evals.py
@@ -15,7 +15,7 @@ def load_custom_model(
 
     # Load model from the specific checkpoint
     model = (
-        AutoModelForCausalLM.from_pretrained(checkpoint_path)
+        AutoModelForCausalLM.from_pretrained(checkpoint_path, torch_dtype=t.bfloat16)
         .to(t.device(device))
         .eval()
     )
@@ -33,7 +33,7 @@ def evaluate_generations(eval_model, tokenizer, texts):
         # Assume the eval_model outputs a single value as a score, adapt as necessary
         with t.no_grad():
             outputs = eval_model.generate(
-                input_ids, max_length=input_ids.shape[1] + 3
+                input_ids, max_length=input_ids.shape[1] + 2
             )  # +3 for potential numerical output
             decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
             
@@ -77,13 +77,14 @@ def main():
         decoded_outputs = [
             tokenizer.decode(output, skip_special_tokens=True) for output in outputs
         ]
-        print("GENERATION OUTPUT" + str(decoded_outputs))
+        # print("GENERATION OUTPUT" + str(decoded_outputs))
         generations.extend(decoded_outputs)
 
     # Evaluate each generation
     scores = evaluate_generations(eval_model, tokenizer, generations)
 
     # Print or store the results
+    print("FINAL")
     for text, score in zip(generations, scores):
         print(f"Text: {text}, Score: {score}")
 
